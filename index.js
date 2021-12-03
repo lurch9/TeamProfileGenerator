f53407d983684822675594b111dffd56a282ed65
lurch9 <90292267+lurch9@users.noreply.github.com>
2021-12-02 18:10:17 -0800

    added template HTML and wrote newEmployee() defn.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Manager = require("./lib/Manager");
 
 
 const teamMembers = [];
-const createTeam = () => {
+const init = () => {
     inquirer.prompt([
         {
             type: 'name',
@@ -25,10 +25,17 @@ const createTeam = () => {
             message: 'Enter the team manager\'s employee email: \n\n',
             name: 'email'
         },
+        
+        {
+            type: 'input',
+            message: 'Enter the office number: \n\n',
+            name: 'officeNumber'
+        }
 
     ])
-    .then((results) => {
-                teamMembers.push(new Manager(response.title,response.ID,response.email,results.officeNumber));
+    .then((response) => {
+                teamMembers.push(new Manager(response.title,response.ID,response.email,response.officeNumber));
+                console.log('NOW ADDING TEAM MEMBERS: \n\n');
                 newEmployee();
     })
 };
@@ -114,7 +121,71 @@ const newEmployee = () => {
                 newEmployee();
             }
 
-            
+
+            else {
+
+                const templateHTML = `
+                <!DOCTYPE html>
+                <html>
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>Employee Generator</title>
+                    <link href="/assets/styles.css" rel="stylesheet" />
+                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">            
+                  </head>
+                  <body>
+                    <section class="section">
+                        <div class="container">
+                            <h1 class="title">Your Team</h1>
+                            </br>
+                            </br>
+                            <div class="columns is-multiline">
+                                ${teamMembers.map(index => {
+
+                                    return `
+                                    <div class="d-flex justify-content-center section">
+                                        <div class="col-lg-8 col-md-6 col-sm-12 p-2 card">
+                                            <div class="card-content">
+                                                <div class="content">
+                                                    <h3> Name: ${index.name}</h3>
+                                                    <h6>  Role: ${index.getRole()} </h6>
+                                                    <p> ID: ${index.id} </p>
+                                                    <a href="mailto:${index.email}">Email: ${index.email}</a>
+                                                    ${(
+                                                        () => { 
+                                                            switch(index.getRole()){
+                                                                case "Manager":
+                                                                    return `<p>Office Number: ${index.getOfficeNumber()}</p>`;
+                                                                    break;
+                                                                case "Engineer":
+                                                                    return `<a href="https://github.com/${index.getGitHub()}"><p class="github">Github</p> </a>`;
+                                                                    break;
+                                                                case "Intern":
+                                                                    return `<p>School: ${index.getSchool()}</p>`;
+                                                                    break;
+                                                                default:
+                                                                    break;
+                                                            }
+                                                        
+                                                        })
+                                                    ()} 
+                                                                                   
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>`
+                                }).join('')}
+                            </div>
+                        </div>
+                    </section>
+                  </body>
+                </html>
+                `
+            fs.writeFileSync("index.html", templateHTML);
+            }
         })
     })
 };
+
+init();
